3699fc60e4a78d95bb4a20a52e62165ed3cdfff7
Dave Daniels <dave.daniels@daveramsey.com>
2023-08-19 14:42:57 -0500

    feat: Integrate Vue Router and Add Global Styles
    - Integrated Vue Router 4 for routing within the application.
    - Created a router configuration to define routes and associated components.
    - Imported and used Vue Router in the main application file.
    - Added global styles to improve the overall styling of the application.
    
    This commit sets up the foundation for navigation and enhances the visual appearance of the Vue 3 application.

---
+++ web/src/App.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+/* Reset some default styles for consistency */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+/* Apply a dark background to the entire page */
+body {
+    background-color: #121212;
+    /* Dark background color */
+    color: #fff;
+    /* Text color */
+    font-family: Arial, sans-serif;
+    /* Font family for text */
+}
+
+/* Style the app container */
+.app-container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+/* Style the header */
+header {
+    text-align: center;
+    padding: 20px;
+}
+
+header h1 {
+    font-size: 36px;
+}
+
+/* Style the playlist section */
+.playlist-section {
+    margin-bottom: 40px;
+}
+
+.playlist-list {
+    list-style: none;
+    padding: 0;
+}
+
+.playlist-item {
+    margin-bottom: 10px;
+}
+
+.playlist-item a {
+    text-decoration: none;
+    color: #fff;
+    font-size: 18px;
+    display: block;
+}
+
+/* Style the music control bar */
+.music-control-bar {
+    text-align: center;
+}
+
+.music-control-bar button {
+    background-color: #ff5722;
+    /* Play button color */
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    margin: 10px;
+}
+
+/* Style the footer */
+footer {
+    text-align: center;
+    padding: 20px 0;
+    background-color: #333;
+    /* Footer background color */
+}
+
+footer p {
+    color: #fff;
+}
+</style>
---
+++ web/src/components/LandingPage.vue
@@ -0,0 +1,51 @@
+<template>
+        <div class="app-container">
+            <header>
+                <h1>Welcome to My Music App</h1>
+            </header>
+
+            <main>
+                <section class="playlist-section">
+                    <h2>Playlists</h2>
+                    <ul class="playlist-list">
+                        <!-- Replace with dynamically generated playlist items -->
+                        <li class="playlist-item">
+                            <a href="#">Playlist 1</a>
+                        </li>
+                        <li class="playlist-item">
+                            <a href="#">Playlist 2</a>
+                        </li>
+                        <!-- Add more playlist items as needed -->
+                    </ul>
+                </section>
+
+                <section class="music-control-bar">
+                    <!-- Replace with music control buttons (play, pause, etc.) -->
+                    <button id="play-button">Play</button>
+                    <button id="pause-button">Pause</button>
+                    <!-- Add more control buttons and functionality here -->
+                </section>
+            </main>
+        </div>
+
+        <footer>
+            <p>&copy; 2023 My Music App</p>
+        </footer>
+
+        <!-- <script src="app.js"></script> -->
+</template>
+
+<script>
+export default {
+    name: 'LandingPage',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+/* Add more custom styles as needed */</style>
