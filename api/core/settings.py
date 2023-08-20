d3f2bf19488eca3ce0387bcc5e1439c8f5db2460
Dave Daniels <dave.daniels@daveramsey.com>
2023-08-20 12:04:17 -0500

    feat: Serve static MP3 files
    Configured Django to serve static MP3 files from the '/mp3/' URL path. MP3 files are stored in the 'mp3' directory within the project. Added necessary settings and updated the URL configuration to make MP3 files accessible to users.

--- api/core/settings.py
+++ api/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings. This would be restricted in a prod env.
 CORS_ALLOW_ALL_ORIGINS = True
+
+STATIC_URL = '/mp3/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "mp3"),
+]
