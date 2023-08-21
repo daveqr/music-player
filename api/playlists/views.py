2c701eb11359d81a3abc69018fab9f79c879c763
Dave Daniels <dave.daniels@daveramsey.com>
2023-08-20 21:43:30 -0500

    Update track information to use ficticious titles and band names

--- api/playlists/views.py
+++ api/playlists/views.py
@@ -6,17 +6,17 @@ from django.conf import settings
 
 def get_playlists(request):
     track1 = Track.objects.create(
-        artist="StudioKolomna", title="Risk", url=f"{settings.BASE_URL}/StudioKolomna_risk.mp3")
+        artist="Neon Octopus", title="Electric Tentacles", url=f"{settings.BASE_URL}/StudioKolomna_risk.mp3")
     track2 = Track.objects.create(
-        artist="Lexin Music", title="Inspiring Cinematic Ambient", url=f"{settings.BASE_URL}/lexin_inspiring-cinematic-ambient.mp3")
+        artist="Frozen Fire", title="Melting Dreams", url=f"{settings.BASE_URL}/lexin_inspiring-cinematic-ambient.mp3")
     track3 = Track.objects.create(
-        artist="Universefield", title="Creepy Piano", url=f"{settings.BASE_URL}/UNIVERSFIELD_creepy-piano.mp3")
+        artist="Shadowplay Ensemble", title="Whispering Shadows", url=f"{settings.BASE_URL}/UNIVERSFIELD_creepy-piano.mp3")
     track4 = Track.objects.create(
-        artist="Keyfram Audio", title="Now This", url=f"{settings.BASE_URL}/now-this-Keyframe_Audio.mp3")
+        artist="Astrofunk Odyssey", title="Alien Funk Encounter", url=f"{settings.BASE_URL}/now-this-Keyframe_Audio.mp3")
     track5 = Track.objects.create(
-        artist="Fassounds", title="Good Night", url=f"{settings.BASE_URL}/fassounds_good-night.mp3")
+        artist="Velvet Thunderstruck", title="Good Night", url=f"{settings.BASE_URL}/fassounds_good-night.mp3")
     track56 = Track.objects.create(
-        artist="Leva", title="Eternity", url=f"{settings.BASE_URL}/leva_eternity.mp3")
+        artist="Galactic Groove Collective", title="Eternity", url=f"{settings.BASE_URL}/leva_eternity.mp3")
 
     cinematicPl = Playlist.objects.create(
         name="Cinematic", description="Cinematic tracks")
