ff807a2faf7ee5da0c0f8d6118db6e77a5c5da40
Dave Daniels <dave.daniels@daveramsey.com>
2023-08-22 07:48:45 -0500

    feat: Implement MIME type check for uploaded files using python-magic
    Utilized the python-magic library to accurately determine the MIME type
    of uploaded files

--- api/uploads/views.py
+++ api/uploads/views.py
@@ -1,7 +1,8 @@
-from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ViewSet
+import magic
+import tempfile
 
 
 class UploadView(ViewSet):
@@ -10,7 +11,21 @@ class UploadView(ViewSet):
 
     def create(self, request):
         file_uploaded = request.FILES.get('file_uploaded')
-        content_type = file_uploaded.content_type
-        response = f"File uploaded successfully {content_type}".format(
-            content_type)
-        return Response(response)
+
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            for chunk in file_uploaded.chunks():
+                temp_file.write(chunk)
+
+        mime = magic.Magic(mime=True)
+        file_type = mime.from_file(temp_file.name)
+
+        if "audio/mpeg" in file_type:
+            response = f"File uploaded successfully: {file_type}"
+            return Response(response)
+        else:
+            response = f"File is not an MP3. Detected type: {file_type}"
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+        # response = f"File uploaded successfully {file_type}".format(
+        #     file_type)
+        # return Response(response)
