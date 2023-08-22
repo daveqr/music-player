dad27f77deb11363b416180f97c00775233aef67
Dave Daniels <dave.daniels@daveramsey.com>
2023-08-22 15:02:24 -0500

    feat: add rest authentication via dj-rest-auth library

--- api/core/settings.py
+++ api/core/settings.py
@@ -39,16 +39,27 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'dj_rest_auth.registration',    
     'corsheaders',
+
     'playlists',
-    'uploads'
+    'uploads',  
 ]
+
+SITE_ID = 1
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',    
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
--- api/core/urls.py
+++ api/core/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+
     path('playlists/', include('playlists.urls')),
     path('upload/', include('uploads.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
